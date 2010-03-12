019fe444dff8bb090c1634ee2cada094fe771599
Dremov Kirill (Nokia-D-MSW/Tampere) <kirill.dremov@nokia.com>
2010-03-12 15:43:48 +0200

    Revision: 201007 Kit: 201008
    --HG--
    branch : RCL_3

--- data/Create_GStreamer_STUB_SIS.bat
+++ data/Create_GStreamer_STUB_SIS.bat
@@ -1,16 +1,21 @@
 rem
-rem Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
-rem All rights reserved.
-rem This component and the accompanying materials are made available
-rem under the terms of the License "Eclipse Public License v1.0"
-rem which accompanies this distribution, and is available
-rem at the URL "http://www.eclipse.org/legal/epl-v10.html".
+rem Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies). All rights reserved.
 rem
-rem Initial Contributors:
-rem Nokia Corporation - initial contribution.
+rem This library is free software; you can redistribute it and/or
+rem modify it under the terms of the GNU Lesser General Public
+rem License as published by the Free Software Foundation; either
+rem version 2 of the License, or (at your option) any later version.
 rem
-rem Contributors:
+rem This library is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+rem Lesser General Public License for more details.
 rem
+rem You should have received a copy of the GNU Lesser General Public
+rem License along with this library; if not, write to the
+rem Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+rem Boston, MA 02111-1307, USA.
+rem 
 rem Description: PKG for GStreamer
 rem
 
